3a20e80cf76b329283bf0c5fb2d9dba11b9e248a
K0R0B0R0S <joseasfer07@gmail.com>
2024-12-01 14:06:32 -0300

    feat: tests for daos

--- src/dao/appointments_dao.rs
+++ src/dao/appointments_dao.rs
@@ -41,4 +41,56 @@ impl AppointmentsDAO {
         self.collection.delete_one(filter).await?;
         Ok(())
     }
+
+    pub async fn read_by_id(&self, appointment: &Appointments) -> Result<Option<Appointments>, Box<dyn std::error::Error>> {
+        let filter = bson::doc! { "_id": appointment.id };
+        let result = self.collection.find_one(filter).await?;
+        Ok(result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::services::database::connect;
+    use crate::models::appointments::Prioridade;
+    use dotenvy::dotenv;
+
+    #[tokio::test]
+    async fn test_create_read_update_delete_appointment() {
+        dotenv().ok();
+        let db = connect().await.unwrap();
+        let dao = AppointmentsDAO::new(&db).unwrap();
+
+        let appointment = Appointments {
+            id: bson::oid::ObjectId::new(),
+            titulo: "Test Appointment".to_string(),
+            descricao: "This is a test appointment".to_string(),
+            data: bson::DateTime::from_chrono(chrono::Utc::now()),
+            hora: "10:00".to_string(),
+            prioridade: Prioridade::Alta,
+            duracao: 20
+        };
+
+        dao.create(appointment.clone()).await.unwrap();
+
+        let appointment_option = dao.read_by_id(&appointment).await.unwrap();
+        assert!(appointment_option.is_some());
+        let mut appointment = appointment_option.unwrap();
+        assert_eq!(appointment.titulo, "Test Appointment");
+
+        appointment.titulo = "Updated Test Appointment".to_string();
+        dao.update(appointment.clone()).await.unwrap();
+
+        let updated_appointment_option = dao.read_by_id(&appointment).await.unwrap();
+        assert!(updated_appointment_option.is_some());
+        let updated_appointment = updated_appointment_option.unwrap();
+        assert_eq!(updated_appointment.titulo, "Updated Test Appointment");
+
+
+        dao.delete(&appointment).await.unwrap();
+
+        let deleted_appointment_option = dao.read_by_id(&appointment).await.unwrap();
+        assert!(deleted_appointment_option.is_none());
+    }
 }
--- src/dao/contacts_dao.rs
+++ src/dao/contacts_dao.rs
@@ -41,4 +41,57 @@ impl ContactsDAO {
         self.collection.delete_one(filter).await?;
         Ok(())
     }
+
+    pub async fn read_by_id(&self, contact: &Contacts) -> Result<Option<Contacts>, Box<dyn std::error::Error>> {
+        let filter = bson::doc! { "_id": contact.id };
+        let result = self.collection.find_one(filter).await?;
+        Ok(result)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::services::database::connect;
+    use crate::models::contacts::ContactCategory;
+    use dotenvy::dotenv;
+
+
+    #[tokio::test]
+    async fn test_create_read_update_delete_contact() {
+        dotenv().ok();
+        let db = connect().await.unwrap();
+        let dao = ContactsDAO::new(&db).unwrap();
+
+        let contact = Contacts {
+            id: bson::oid::ObjectId::new(),
+            name: "Test Contact".to_string(),
+            email: "test@example.com".to_string(),
+            phone: "123456789".to_string(),
+            age: 0,
+            date_of_birth: bson::DateTime::from_chrono(chrono::Utc::now() - chrono::Duration::days(1)),
+            category: ContactCategory::Family,
+        };
+
+        dao.create(contact.clone()).await.unwrap();
+
+        let contact_option = dao.read_by_id(&contact).await.unwrap();
+        assert!(contact_option.is_some());
+        let mut contact = contact_option.unwrap();
+        assert_eq!(contact.name, "Test Contact");
+
+        contact.name = "Updated Test Contact".to_string();
+        dao.update(contact.clone()).await.unwrap();
+
+        let updated_contact_option = dao.read_by_id(&contact).await.unwrap();
+        assert!(updated_contact_option.is_some());
+        let updated_contact = updated_contact_option.unwrap();
+        assert_eq!(updated_contact.name, "Updated Test Contact");
+
+        dao.delete(&contact).await.unwrap();
+
+        let deleted_contact_option = dao.read_by_id(&contact).await.unwrap();
+        assert!(deleted_contact_option.is_none());
+    }
 }
